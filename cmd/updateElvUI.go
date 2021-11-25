f94c31b0c6de5c88fc7328ae6c644dcdc15a1d37
NollyTV <77259260+NollyTV@users.noreply.github.com>
2021-11-25 11:11:49 -0500

    Add updateElvUI.go file

---
+++ cmd/updateElvUI.go
@@ -0,0 +1,7 @@
+package cmd
+
+// Check current elvui version in C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\ElvUI\ElvUI_Mainline.toc
+
+// Compare it with current released version at https://www.tukui.org/download.php?ui=elvui
+
+// if version is newer, zip up old installation and unzip new one.
