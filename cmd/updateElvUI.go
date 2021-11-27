9739fd90d4f235285b81ad5165ed9104c5d8d86b
ldougbmx <77259260+NollyTV@users.noreply.github.com>
2021-11-26 20:07:52 -0500

    Add version check, zip step, and download step

--- cmd/updateElvUI.go
+++ cmd/updateElvUI.go
@@ -2,8 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"strings"
+	"time"
 	"wowtools/utilities"
+
+	"github.com/schollz/progressbar/v3"
+	"github.com/spf13/viper"
 )
 
 func UpdateElvUI() {
@@ -14,11 +19,40 @@ func UpdateElvUI() {
 	fmt.Println(latestVersion)
 	if latestVersion > stringCurrentVersion {
 		fmt.Printf("A later version of ElvUI is available. Current version: %s; New version: %s\n", stringCurrentVersion, latestVersion)
+		updatePrompt := utilities.AskForConfirmation("Do you want to install the lastest version of ElvUI?")
+		if updatePrompt {
+			fmt.Println("Zipping current ElvUI folder")
+			ZipElvUI()
+			fmt.Printf("Downloading ElvUI %s\n", latestVersion)
+			DownloadElvUI(latestVersion)
+			// if version is newer, zip up old installation and unzip new one.
+		}
 	} else {
 		fmt.Println("ElvUI is up to date")
 	}
 }
 
-// Compare it with current released version at https://www.tukui.org/download.php?ui=elvui
+func ZipElvUI() {
+	elvuiFolder := viper.GetString("elvui_dir")
+	backupFolder := viper.GetString("backup_dir") + "ElvUI\\"
+	currentTime := time.Now()
+	folderName := currentTime.Format("2006-01-02")
 
-// if version is newer, zip up old installation and unzip new one.
+	fmt.Println("Beginning backup of WTF folder")
+	if err := utilities.ZipSource(elvuiFolder, backupFolder+folderName+".zip"); err != nil {
+		log.Fatal(err)
+	}
+	// Not really a true progress bar at the moment - more of a visual for the user - need to reseach better implementation, but works for now, as the zip process is fairly quick for the WTF folder
+	bar := progressbar.Default(100)
+	for i := 0; i < 100; i++ {
+		bar.Add(1)
+		time.Sleep(20 * time.Millisecond)
+	}
+	fmt.Println("Folder backup complete")
+}
+
+func DownloadElvUI(latestVersion string) {
+	downloadUri := "https://www.tukui.org/downloads/elvui-" + latestVersion + ".zip"
+	fmt.Println(downloadUri)
+	utilities.DownloadFiles(downloadUri)
+}
