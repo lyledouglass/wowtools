95a3dedc9ba41907dca4f0362b0402cf5263110e
ldougbmx <77259260+NollyTV@users.noreply.github.com>
2021-11-26 20:07:25 -0500

    Create fileHelper.go

---
+++ utilities/fileHelper.go
@@ -0,0 +1,77 @@
+package utilities
+
+import (
+	"archive/zip"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func ZipSource(source, target string) error {
+	f, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	writer := zip.NewWriter(f)
+	defer writer.Close()
+
+	return filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+		header.Method = zip.Deflate
+		header.Name, err = filepath.Rel(filepath.Dir(source), path)
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			header.Name += "/"
+		}
+
+		headerWriter, err := writer.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		_, err = io.Copy(headerWriter, f)
+		return err
+	})
+}
+
+func DownloadFiles(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Fatal(resp.StatusCode)
+	}
+
+	fileOutput, err := os.Create("ElvUI.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fileOutput.Close()
+
+	_, err = io.Copy(fileOutput, resp.Body)
+	return err
+}
