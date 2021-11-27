87fd896e1ee53b7c6dab6768cdc0bfeeb06832b8
ldougbmx <77259260+NollyTV@users.noreply.github.com>
2021-11-26 21:19:08 -0500

    Added Remove and Unzip functions

--- utilities/fileHelper.go
+++ utilities/fileHelper.go
@@ -2,11 +2,13 @@ package utilities
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func ZipSource(source, target string) error {
@@ -79,4 +81,61 @@ func DownloadFiles(filename string, url string) error {
 
 	_, err = io.Copy(fileOutput, resp.Body)
 	return err
+}
+
+func RemoveFolder(folderPath string) {
+	err := os.RemoveAll(folderPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Unzip(src string, dest string) ([]string, error) {
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		fpath := filepath.Join(dest, f.Name)
+
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s: illegal file path", fpath)
+		}
+
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return filenames, err
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return filenames, err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return filenames, err
+		}
+	}
+	return filenames, nil
 }
