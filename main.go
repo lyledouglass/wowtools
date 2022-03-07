037a888151b3e5f2d08bd6abdb628a865f41b65e
ldougbmx <77259260+ldougbmx@users.noreply.github.com>
2022-03-07 00:10:14 -0500

    Add WaitGroup (#11)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync"
 	"wowtools/cmd"
 	"wowtools/utilities"
 
@@ -9,9 +10,15 @@ import (
 
 func main() {
 	cmd.InitConfig()
-	utilities.VerifyFolders(viper.GetString("backup_dir"))
-	go utilities.VerifyFolders(viper.GetString("backup_dir") + "ElvUI")
-	go utilities.VerifyFolders(viper.GetString("backup_dir") + "WTF")
+
+	// WaitGroup for creating missing folders.
+	var wg sync.WaitGroup
+	wg.Add(2)
+	utilities.VerifyFolders(viper.GetString("backup_dir"), &wg)
+	go utilities.VerifyFolders(viper.GetString("backup_dir")+"ElvUI", &wg)
+	go utilities.VerifyFolders(viper.GetString("backup_dir")+"WTF", &wg)
+	wg.Wait()
+
 	cmd.WtfBackup()
 	cmd.UpdateElvUI()
 	utilities.OpenCurseforge()
