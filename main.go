f891d2b9f9caec326d2815bf2226f98611fad6a9
ldougbmx <lyled93@gmail.com>
2022-03-08 17:18:21 -0500

    Add missing go routine and up waitgroup number

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ func main() {
 
 	// WaitGroup for creating missing folders.
 	var wg sync.WaitGroup
-	wg.Add(2)
-	utilities.VerifyFolders(viper.GetString("backup_dir"), &wg)
+	wg.Add(3)
+	go utilities.VerifyFolders(viper.GetString("backup_dir"), &wg)
 	go utilities.VerifyFolders(viper.GetString("backup_dir")+"ElvUI", &wg)
 	go utilities.VerifyFolders(viper.GetString("backup_dir")+"WTF", &wg)
 	wg.Wait()
