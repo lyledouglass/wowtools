b80324cdfc841f3386082e3cafd4dbf97e1a6735
lyledouglass <lyle.douglass@gmail.com>
2022-05-14 10:30:55 -0400

    Add GetPublishedAppVersion and GetCurrentAppVersion

--- utilities/versionHelper.go
+++ utilities/versionHelper.go
@@ -2,10 +2,15 @@ package utilities
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/gonutz/w32/v2"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -43,4 +48,57 @@ func GetLatestVersion() string {
 		log.Fatal(jsonErr)
 	}
 	return (data.Version)
+}
+
+type githubApiData struct {
+	AppVersion string `json:"tag_name"`
+}
+
+func GetPublishedAppVersion() string {
+	url := "https://api.github.com/repos/lyledouglass/wowtools/releases/latest"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data githubApiData
+	jsonErr := json.Unmarshal(body, &data)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	return strings.Trim(data.AppVersion, "{ v }")
+}
+
+func GetCurrentAppVersion() string {
+	executablePath, err := os.Executable()
+	if err != nil {
+		log.Fatalln("Failed to get path of executable")
+	}
+	size := w32.GetFileVersionInfoSize(executablePath)
+	if size <= 0 {
+		log.Fatalln("GetFileVersionInfoSize failed")
+	}
+	info := make([]byte, size)
+	getFileInfo := w32.GetFileVersionInfo(executablePath, info)
+	if !getFileInfo {
+		log.Fatalln("GetFileVersionInfo failed")
+	}
+	fixed, getFileInfo := w32.VerQueryValueRoot(info)
+	if !getFileInfo {
+		log.Fatalln("VerQueryValueRoot failed")
+	}
+	fileVersion := fixed.FileVersion()
+	versionInt, err := fmt.Printf("%d.%d.%d.%d\n",
+		fileVersion&0xFFFF000000000000>>48,
+		fileVersion&0x0000FFFF00000000>>32,
+		fileVersion&0x00000000FFFF0000>>16,
+		fileVersion&0x000000000000FFFF>>0)
+	if err != nil {
+		log.Fatalln("Failed to assign version to string")
+	}
+	versionString := strconv.Itoa(versionInt)
+	return versionString
 }
