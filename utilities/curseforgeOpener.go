80376b086f91de105ef3f209efa5a338f242c612
lyledouglass <lyle.douglass@gmail.com>
2022-05-16 11:53:41 -0400

    Updates for CurseForge standalone install, depreciation of Overwolf method

--- utilities/curseforgeOpener.go
+++ utilities/curseforgeOpener.go
@@ -10,13 +10,11 @@ import (
 
 func OpenCurseforge() {
 	curseforgeExe := viper.GetString("curseforge_exe")
-	curseforgeArgs := viper.GetString("curseforge_args")
 	updatePrompt := AskForConfirmation("Do you want to launch Curseforge to update addons?")
 	if updatePrompt {
 		fmt.Println("Opening Cureseforge")
 		cmd := exec.Command("powershell", "Start-Process",
-			fmt.Sprintf("-Filepath '%s'", curseforgeExe),
-			fmt.Sprintf("-ArgumentList '%s'", curseforgeArgs))
+			fmt.Sprintf("-Filepath '%s'", curseforgeExe))
 		err := cmd.Start()
 		if err != nil {
 			log.Fatal(err)
