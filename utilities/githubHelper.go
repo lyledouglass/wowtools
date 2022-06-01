5c2f5806c51ce38dea9958315379f282c1f2c450
lyledouglass <lyle.douglass@gmail.com>
2022-05-31 23:02:44 -0400

    Create githubHelper.go to get asset name + download uri

---
+++ utilities/githubHelper.go
@@ -0,0 +1,46 @@
+package utilities
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func GetReleaseAsset(uri string, assetName string) string {
+	var downloadUri string
+	type githubApiData struct {
+		AppVersion string `json:"tag_name"`
+		Assets     []struct {
+			Name               string `json:"name"`
+			BrowserDownloadURL string `json:"browser_download_url"`
+		}
+	}
+	resp, err := http.Get(uri)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data githubApiData
+	jsonErr := json.Unmarshal(body, &data)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	for _, asset := range data.Assets {
+		if asset.Name == assetName {
+			downloadUri = asset.BrowserDownloadURL
+		} else {
+			log.Fatal("Unable to get download URI for asset. Verify asset name and api uri are correct")
+		}
+	}
+	return downloadUri
+}
