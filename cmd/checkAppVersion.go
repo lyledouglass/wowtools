9139cd6e5392293f19be38f5288a16db0bde14f2
lyledouglass <lyle.douglass@gmail.com>
2022-06-01 00:08:36 -0400

    Update text output

--- cmd/checkAppVersion.go
+++ cmd/checkAppVersion.go
@@ -10,10 +10,10 @@ import (
 const wowtoolsUri = "https://api.github.com/repos/lyledouglass/wowtools/releases/latest"
 
 var latestVersion = utilities.GetPublishedAppVersion(wowtoolsUri)
+var currentVersion = utilities.GetCurrentAppVersion()
 
 func compareAppVersioning() bool {
 	var updateApp bool
-	currentVersion := utilities.GetCurrentAppVersion()
 	if currentVersion > latestVersion {
 		updateApp = true
 	}
@@ -23,7 +23,8 @@ func compareAppVersioning() bool {
 func UpdateWowtools() {
 	updateApp := compareAppVersioning()
 	if updateApp == true {
-		updatePrompt := utilities.AskForConfirmation("You are running on an older version of this application. Would you like to download the latest version?")
+		fmt.Printf("You are running on an older version (%s) of this application. Would you like to download the latest version (%s)?", currentVersion, latestVersion)
+		updatePrompt := utilities.AskForConfirmation("")
 		if updatePrompt {
 			fmt.Println("Downloading latest package...")
 			downloadUri := utilities.GetReleaseAsset(wowtoolsUri, "wowtools.exe")
@@ -33,6 +34,7 @@ func UpdateWowtools() {
 			}
 			homeDir, _ := os.UserHomeDir()
 			fmt.Printf("Wowtools version %s hase been downloaded to %s. Please close this application and replace it with the new executable", latestVersion, homeDir+"\\Downloads\\")
+			fmt.Println("")
 			fmt.Println("Press 'Enter' to close the program to update")
 			var input string
 			fmt.Scanln(&input)
