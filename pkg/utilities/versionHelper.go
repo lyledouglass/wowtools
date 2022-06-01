538f5539fe9946570e541f8a4dea59c83b56b5f5
lyledouglass <lyle.douglass@gmail.com>
2022-06-01 14:36:29 -0400

    Move utilities to pkg

--- pkg/utilities/versionHelper.go
+++ pkg/utilities/versionHelper.go
@@ -8,46 +8,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 	"strings"
-
-	"github.com/spf13/viper"
 )
-
-func GetCurrentVersion() []string {
-	elvuiDir := viper.GetString("elvui_dir")
-	fileOpen, err := ioutil.ReadFile(elvuiDir + "ElvUI_Mainline.toc")
-	if err != nil {
-		log.Fatal(err)
-	}
-	str := string(fileOpen)
-	re := regexp.MustCompile(`[0-9]+\.[0-9]+`)
-	v := re.FindStringSubmatch(str)
-
-	return v
-}
-
-type apiData struct {
-	Version string `json:"version"`
-}
-
-func GetLatestVersion() string {
-	url := "https://www.tukui.org/api.php?ui=elvui"
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var data apiData
-	jsonErr := json.Unmarshal(body, &data)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-	return (data.Version)
-}
 
 type githubApiData struct {
 	AppVersion string `json:"tag_name"`
@@ -75,6 +37,7 @@ func GetCurrentAppVersion() string {
 	if err != nil {
 		log.Fatalln("Failed to get path of executable")
 	}
+	fmt.Println(executablePath)
 	size := w32.GetFileVersionInfoSize(executablePath)
 	if size <= 0 {
 		log.Fatalln("GetFileVersionInfoSize failed")
