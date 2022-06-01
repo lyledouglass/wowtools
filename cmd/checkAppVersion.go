473a192d6591d347169d960fe069fe31e72508c6
lyledouglass <lyle.douglass@gmail.com>
2022-05-31 23:22:43 -0400

    Add logic to download latest version and close the program for update.

--- cmd/checkAppVersion.go
+++ cmd/checkAppVersion.go
@@ -1,16 +1,44 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"wowtools/utilities"
 )
 
-func compareAppVersioning() {
+const wowtoolsUri = "https://api.github.com/repos/lyledouglass/wowtools/releases/latest"
+
+var latestVersion = utilities.GetPublishedAppVersion(wowtoolsUri)
+
+func compareAppVersioning() bool {
+	var updateApp bool
 	currentVersion := utilities.GetCurrentAppVersion()
-	latestVersion := utilities.GetPublishedAppVersion()
 	if currentVersion > latestVersion {
+		updateApp = true
+	}
+	return updateApp
+}
+
+func UpdateWowtools() {
+	updateApp := compareAppVersioning()
+	if updateApp == true {
 		updatePrompt := utilities.AskForConfirmation("You are running on an older version of this application. Would you like to download the latest version?")
 		if updatePrompt {
-
+			fmt.Println("Downloading latest package...")
+			downloadUri := utilities.GetReleaseAsset(wowtoolsUri, "wowtools.exe")
+			err := utilities.DownloadFiles("wowtools.exe", downloadUri)
+			if err != nil {
+				log.Fatal("Download step failed")
+			}
+			homeDir, _ := os.UserHomeDir()
+			fmt.Printf("Wowtools version %s hase been downloaded to %s. Please close this application and replace it with the new executable", latestVersion, homeDir+"\\Downloads\\")
+			fmt.Println("Press 'Enter' to close the program to update")
+			var input string
+			fmt.Scanln(&input)
+			os.Exit(0)
 		}
+	} else {
+		fmt.Println("wowtools is up to date, continuing...")
 	}
 }
