1e38cfa768feb130199fc22102e2345aae479da2
Lyle Douglass <lyle.douglass@gmail.com>
2022-07-23 12:32:14 -0400

    Add copyPtr boolVar flag and conditional

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/spf13/viper"
 	"sync"
 	"wowtools/internal"
@@ -10,19 +11,26 @@ import (
 )
 
 func main() {
-
 	internal.InitConfig()
 	// Check for updates to the application
 	internal.UpdateWowtools()
-	// WaitGroup for creating missing folders.
-	var wg sync.WaitGroup
-	wg.Add(3)
-	go utilities.VerifyFolders(viper.GetString("backup_dir"), &wg)
-	go utilities.VerifyFolders(viper.GetString("backup_dir")+"ElvUI", &wg)
-	go utilities.VerifyFolders(viper.GetString("backup_dir")+"WTF", &wg)
-	wg.Wait()
 
-	internal.WtfBackup()
-	internal.UpdateElvUI()
-	internal.OpenCurseforge()
+	var copyPtr bool
+	flag.BoolVar(&copyPtr, "copy-ptr", false, "only performs copy of PTR folders from Retail")
+	flag.Parse()
+	if copyPtr {
+		internal.CopyPtrData()
+	} else {
+		// WaitGroup for creating missing folders.
+		var wg sync.WaitGroup
+		wg.Add(3)
+		go utilities.VerifyFolders(viper.GetString("backup_dir"), &wg)
+		go utilities.VerifyFolders(viper.GetString("backup_dir")+"ElvUI", &wg)
+		go utilities.VerifyFolders(viper.GetString("backup_dir")+"WTF", &wg)
+		wg.Wait()
+
+		internal.WtfBackup()
+		internal.UpdateElvUI()
+		internal.OpenCurseforge()
+	}
 }
