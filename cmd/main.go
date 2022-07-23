1e56529ec7715f1ebb1ca420e4df0a03282cd72e
Lyle Douglass <lyle.douglass@gmail.com>
2022-07-23 15:00:26 -0400

    Feature: backup only flag (#31)
    * Add backup-only flag and conditional.
    Move WG to run always
    Add conditional to only run full operations when no flags are found
    
    * Update README.md
    
    * Up minor version

--- cmd/main.go
+++ cmd/main.go
@@ -14,21 +14,33 @@ func main() {
 	internal.InitConfig()
 	// Check for updates to the application
 	internal.UpdateWowtools()
+	var (
+		copyPtr    bool
+		backupOnly bool
+	)
 
-	var copyPtr bool
+	// Flags
 	flag.BoolVar(&copyPtr, "copy-ptr", false, "only performs copy of PTR folders from Retail")
+	flag.BoolVar(&backupOnly, "backup-only", false, "perfomrs only backup of wtf folder")
 	flag.Parse()
+
+	// WaitGroup for creating missing folders.
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go utilities.VerifyFolders(viper.GetString("backup_dir"), &wg)
+	go utilities.VerifyFolders(viper.GetString("backup_dir")+"ElvUI", &wg)
+	go utilities.VerifyFolders(viper.GetString("backup_dir")+"WTF", &wg)
+	wg.Wait()
+
 	if copyPtr {
 		internal.CopyPtrData()
-	} else {
-		// WaitGroup for creating missing folders.
-		var wg sync.WaitGroup
-		wg.Add(3)
-		go utilities.VerifyFolders(viper.GetString("backup_dir"), &wg)
-		go utilities.VerifyFolders(viper.GetString("backup_dir")+"ElvUI", &wg)
-		go utilities.VerifyFolders(viper.GetString("backup_dir")+"WTF", &wg)
-		wg.Wait()
+	}
 
+	if backupOnly {
+		internal.WtfBackup()
+	}
+
+	if !copyPtr && !backupOnly {
 		internal.WtfBackup()
 		internal.UpdateElvUI()
 		internal.OpenCurseforge()
