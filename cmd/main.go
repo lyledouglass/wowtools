42c431b8e85fc39a731cbb70ee9f11616af25c8c
Lyle Douglass <lyle.douglass@gmail.com>
2022-08-14 10:22:07 -0400

    Move flags, add noUpdates

--- cmd/main.go
+++ cmd/main.go
@@ -11,18 +11,23 @@ import (
 )
 
 func main() {
-	internal.InitConfig()
-	// Check for updates to the application
-	internal.UpdateWowtools()
 	var (
 		copyPtr    bool
 		backupOnly bool
+		noUpdates  bool
 	)
 
 	// Flags
 	flag.BoolVar(&copyPtr, "copy-ptr", false, "only performs copy of PTR folders from Retail")
 	flag.BoolVar(&backupOnly, "backup-only", false, "perfomrs only backup of wtf folder")
+	flag.BoolVar(&noUpdates, "no-updtaes", false, "skips checking updates for wowtools")
 	flag.Parse()
+
+	internal.InitConfig()
+	// Check for updates to the application
+	if !noUpdates {
+		internal.UpdateWowtools()
+	}
 
 	// WaitGroup for creating missing folders.
 	var wg sync.WaitGroup
