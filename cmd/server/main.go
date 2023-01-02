305c32df48bea77563a1b549173d27063d281258
Lyle Douglass <lyle.douglass@gmail.com>
2023-01-02 17:56:20 -0500

    feat: changes for new API access structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,19 +18,17 @@ func main() {
 	flag.BoolVar(&tokenPriceCheck, "token-price-check", false, "checks the price of the WoW token")
 
 	type config struct {
-		AccessToken string `mapstructure:"blizzard_access_token"`
-		WebhookUri  string `mapstructure:"discord_webhook_url"`
+		AccessToken  string `mapstructure:"blizzard_access_token"`
+		ClientID     string `mapstructure:"blizzard_clientid"`
+		ClientSecret string `mapstructure:"blizzard_clientsecret"`
+		WebhookUri   string `mapstructure:"discord_webhook_url"`
 	}
 	var c config
-	err := viper.Unmarshal(&c)
-	if err != nil {
-		log.Fatal("Error unmarshalling viper config: ", err)
+	unmarshallErr := viper.Unmarshal(&c)
+	if unmarshallErr != nil {
+		log.Fatal("Error unmarshalling viper config: ", unmarshallErr)
 	}
-	accessToken := c.AccessToken
-	discordWebhook := c.WebhookUri
 
-	//if tokenPriceCheck {
-	internal.TokenPriceAlert(accessToken, discordWebhook)
-	//}
-
+	blizzAuth := utilities.GetBlizzApiAuth(c.ClientID, c.ClientSecret)
+	internal.TokenPriceAlert(blizzAuth, c.WebhookUri)
 }
