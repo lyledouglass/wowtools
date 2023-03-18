7b102ae7e08d19d7b982b7d0b05050d501190d30
Lyle Douglass <lyle.douglass@gmail.com>
2023-03-18 17:40:34 -0400

    feat: remove new logic and use Unzip func

--- internal/wtfRestore.go
+++ internal/wtfRestore.go
@@ -1,13 +1,10 @@
 package internal
 
 import (
-	"archive/zip"
 	"fmt"
 	"github.com/spf13/viper"
-	"io"
 	"log"
 	"os"
-	"path/filepath"
 	"wowtools/pkg/utilities"
 )
 
@@ -18,38 +15,16 @@ func WtfRestore(file string) {
 	if updatePrompt {
 		fmt.Println("Continuing")
 		fmt.Println("Removing current WTF folder")
+		retailFolder := viper.GetString("retail_dir")
 		wtfFolder := viper.GetString("wtf_dir")
-		err := os.Remove(wtfFolder)
+		// Delete files recursively in order to delete the
+		err := os.RemoveAll(wtfFolder)
 		if err != nil {
 			log.Fatal()
 		}
 		fmt.Printf("Restoring %s", file)
-		restoreFile := viper.GetString("backup_dir") + "\\wtf\\" + file
-		src, err := zip.OpenReader(restoreFile)
-		if err != nil {
-			log.Fatal()
-		}
-		defer src.Close()
-
-		for _, f := range src.File {
-			zf, err := f.Open()
-			if err != nil {
-				log.Fatal()
-			}
-			defer zf.Close()
-
-			dstPath := filepath.Join(wtfFolder, f.Name)
-			dst, err := os.Create(dstPath)
-			if err != nil {
-				log.Fatal()
-			}
-			defer dst.Close()
-
-			_, err = io.Copy(dst, zf)
-			if err != nil {
-				log.Fatal()
-			}
-		}
+		restoreFile := viper.GetString("backup_dir") + "WTF\\" + file
+		utilities.Unzip(restoreFile, retailFolder)
 	} else {
 		fmt.Println("Exiting")
 		os.Exit(0)
